0952bf47ed093c11a18c9932506e56638c070914
nydomi <nydomi1@gmail.com>
2022-09-30 09:45:46 +0200

    extra

---
+++ szamolas.js
@@ -0,0 +1,12 @@
+console.log("ITT VAGYOK");
+
+function hengerSzam(){
+    let a = parseInt(document.henger.sugar.value);
+    let b = parseInt(document.henger.magassag.value);
+    let terfogat = a*a*Math.PI*b;
+    console.log(terfogat);
+    alert(terfogat)
+
+}
+
+
