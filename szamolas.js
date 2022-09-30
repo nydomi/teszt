63fc0c1804f312832c2e52b1857f10b04a4ee1af
nydomi <nydomi1@gmail.com>
2022-09-30 11:05:52 +0200

    bankki kamat számítás

--- szamolas.js
+++ szamolas.js
@@ -10,3 +10,15 @@ function hengerSzam(){
 }
 
 
+function bankSzam(){
+    
+    let penzSzam =   parseFloat(document.bank.money.value);
+    let kamatSzam =parseFloat( document.bank.kamat.value);
+    let honapSzam = parseFloat(document.bank.honap.value);
+    let penzEretk = penzSzam*Math.pow(1+kamatSzam/100,honapSzam/12);
+    console.log("Honapszam: "+honapSzam);
+    console.log(penzEretk);
+
+
+}
+
